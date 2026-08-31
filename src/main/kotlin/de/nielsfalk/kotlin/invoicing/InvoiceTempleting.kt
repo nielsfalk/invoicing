ed7ac7bc9a7cc0569ab9a766ad6f37d02db3a5ff
Niels Falk <niels@nielsfalk.de>
2026-08-31 15:45:19 +0200

    implement main method

--- src/main/kotlin/de/nielsfalk/kotlin/invoicing/InvoiceTempleting.kt
+++ src/main/kotlin/de/nielsfalk/kotlin/invoicing/InvoiceTempleting.kt
@@ -1,5 +1,39 @@
 package de.nielsfalk.kotlin.invoicing
 
-fun main() {
-    //TODO generate invoices in the out folder that are not existing yet if a force argument is given overwrite them
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.Attributes
+import org.asciidoctor.Options
+import org.asciidoctor.SafeMode
+import java.io.File
+import kotlin.use
+
+fun main(args: Array<String>) {
+    val force = args.contains("force") || args.contains("--force")
+    val outDir = File("out")
+    if (!outDir.exists()) outDir.mkdirs()
+
+    Asciidoctor.Factory.create().use { ad ->
+        invoices.forEach { invoice ->
+            val file = File(outDir, invoice.filename)
+            if (!file.exists() || force) {
+                println("Generating ${invoice.filename}...")
+                ad.convert(
+                    invoice.formatAdoc(),
+                    Options.builder()
+                        .backend("pdf")
+                        .safe(SafeMode.UNSAFE)
+                        .attributes(
+                            Attributes.builder()
+                                .attribute("pdf-themesdir", File(".").absolutePath)
+                                .attribute("pdf-theme", "invoice")
+                                .build()
+                        )
+                        .toFile(file)
+                        .build()
+                )
+            } else {
+                println("Skipping ${invoice.filename} (already exists).")
+            }
+        }
+    }
 }
