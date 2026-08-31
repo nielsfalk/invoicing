15496904a347d6b67a22301eaf68e8c6afc9fa69
Niels Falk <niels@nielsfalk.de>
2026-08-31 10:33:55 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    id("io.github.nielsfalk.datatable") version "0.2.1"
+    application
+}
+
+group = "de.nielsfalk.kotlin.invoicing"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.asciidoctor:asciidoctorj:3.0.1")
+    implementation("org.asciidoctor:asciidoctorj-pdf:2.3.23")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("generateDataTables")
+}
+
+application {
+    mainClass = "de.nielsfalk.kotlin.invoicing.InvoiceTempletingKt"
+}
---
+++ src/main/kotlin/de/nielsfalk/kotlin/invoicing/InvoiceTempleting.kt
@@ -0,0 +1,4 @@
+package de.nielsfalk.kotlin.invoicing
+
+fun main() {
+}
